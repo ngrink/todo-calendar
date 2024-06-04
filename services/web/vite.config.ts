9568af9150630bf77d18e9fb3bebb49520b1977a
Nikolay Grinko <ngrink.io@gmail.com>
2024-06-04 14:41:42 +0300

    chore: add path aliases and libraries

--- services/web/vite.config.ts
+++ services/web/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    modules: {
+      localsConvention: 'camelCaseOnly'
+    }
+  },
+  resolve: {
+    alias: {
+      "@/screens": path.resolve(__dirname, './src/views/screens'),
+      "@/components": path.resolve(__dirname, './src/views/components'),
+      "@": path.resolve(__dirname, './src')
+    }
+  }
 })
